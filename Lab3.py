1059f6a14f34cd694950f3e1bda7a7265bc9003a
wei le <WEILE25.24@ichat.sp.edu.sg>
2025-05-12 22:48:20 +0800

    added lab3 with req

--- Lab3.py
+++ Lab3.py
@@ -11,35 +11,44 @@ def bubble_sort(arr, sorting_order):
 
     # Get number of elements in the list
     n = len(arr_result)
+    if all(isinstance(item,int)for item in arr):
+        
+        if 0 < n < 10:
+            # Traverse through all array elements
+            for i in range(n - 1):
+                # range(n) also work but outer loop will
+                # repeat one time more than needed.
 
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+                # Last i elements are already in place
+                for j in range(0, n - i - 1):
 
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
-
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    if sorting_order == SORT_ASCENDING:
+                        if arr_result[j] > arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+                    elif sorting_order == SORT_DESCENDING:
+                        if arr_result[j] < arr_result[j + 1]:
+                            arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
-                else:
-                    # Return an empty array
-                    arr_result = []
+                    else:
+                        # Return an empty array
+                        arr_result = []
+        elif n >= 10:
+            arr_result = 1
+            return arr_result
+        elif n == 0:
+            arr_result = 0
+        return arr_result
     else:
-        arr_result = -1
-
+        arr_result=2
     return arr_result
+ 
+
 
 def main():
     # Driver code to test above
+    
     arr = [64, 34, 25, 12, 22, 11, 90]
 
     # Sort in ascending order
