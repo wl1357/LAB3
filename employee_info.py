3cb434ce3204a662b2bd648b34d05b958ed7dd9c
wei le <WEILE25.24@ichat.sp.edu.sg>
2025-05-29 11:56:55 +0800

    added the stuff

--- employee_info.py
+++ employee_info.py
@@ -23,15 +23,18 @@ def calculate_average_salary():
     average = 0
 
     #add your implementation to calculate here
-
-
-    return average
+    for item in employee_data:
+        total+= item["salary"]
+    average=total/len(employee_data)
+    return round(average,2)
 
 def get_employees_by_dept(department):
     result = []
 
     # Add your implementation from here
-
+    for item in employee_data:
+        if item["department"]==department:
+            result.append(item)
 
     return result
 
